3f0e458e9c863b310456c8fd56326a7e5259dc01
SamuelSimun <samkosimun@gmail.com>
2026-08-03 23:24:27 +0200

    add godrays and improve clouds

--- Scripts/UiScripts/mainMenu.gd
+++ Scripts/UiScripts/mainMenu.gd
@@ -7,7 +7,7 @@ func _ready() -> void:
 
 func _on_new_game() -> void:
 	GameState.current = GameState.State.PLAYING
-	get_tree().change_scene_to_file("res://Maps/testVisuals.tscn")
+	get_tree().change_scene_to_file("res://Maps/testMechanics.tscn")
 
 func _on_quit() -> void:
 	get_tree().quit()
